46836f3c86138654340c0aa90e54108d380eeab1
Dev Bahl <devbahl12@gmail.com>
2020-02-25 23:23:53 +0530

    fourth commit

---
+++ app/src/main/java/com/dbsrm/todolistdsc/splashscreen.kt
@@ -0,0 +1,34 @@
+package com.dbsrm.todolistdsc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splashscreen.*
+
+class splashscreen : AppCompatActivity() {
+
+    override fun onDestroy() {
+        super.onDestroy()
+        overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        imageView6.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_in))
+        Handler().postDelayed({
+            imageView6.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_out))
+            Handler().postDelayed({
+                imageView6.visibility = View.GONE
+                startActivity(Intent(this,walkthrmain::class.java))
+                finish()
+            },1000)
+        },2000)
+    }
+}
